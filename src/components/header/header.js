ea36d88add72e43b8a9b2f034e71d5a866755dee
jasmineblinks <puenehfaith@gmail.com>
2021-05-05 17:04:24 +0100

    style: updated the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,34 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+
 import classes from "./header.module.css";
+import { FaTimes, FaAlignRight } from "react-icons/fa";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={classes.container}>
-      <div className={classes.mobile}>
+      <section className={classes.mobile}>
         <header className={classes.header}>
-          <div className={classes.hero}>
+          <section className={classes.hero}>
             <h2>LOGOS</h2>
-          </div>
-          <div className={classes.menu}>
-            <ul className={classes.listItem}>
-              <li>Features</li>
-              <li>Pricing</li>
-              <li>Resources</li>
-            </ul>
-          </div>
+          </section>
+
+          {isOpen !== true ? (
+            <FaAlignRight
+              size="35px"
+              style={{
+                color: "#fbec3e",
+                border: "2px solid  #fbec3e",
+                // display: "block",
+                marginRight: "25px",
+                // marginTop: "10px",
+              }}
+              onClick={() => setIsOpen(true)}></FaAlignRight>
+          ) : (
+            <FaTimes
+              size="30px"
+              style={{
+                color: "#fbec3e",
+                border: "2px solid  #fbec3e",
+                // display: "block",
+                marginRight: "25px",
+                // marginTop: "15px",
+              }}
+              onClick={() => setIsOpen(false)}></FaTimes>
+          )}
+          {isOpen && (
+            <section className={classes.menu}>
+              <ul className={classes.listItem}>
+                {/* <a href={"https://google.com"}> */}
+                <li>Features</li>
+                {/* </a> */}
+                <li>Pricing</li>
+                <li>Resources</li>
+              </ul>
+            </section>
+          )}
         </header>
-      </div>
-      <div className={classes.desktop}>
+      </section>
+      <section className={classes.desktop}>
         <header className={classes.header}>
-          <div className={classes.hero}>
+          <section className={classes.hero}>
             <h2>LOGOS</h2>
-          </div>
+          </section>
           <ul className={classes.listItem}>
-            <li>features</li>
-            <li>pricing</li>
-            <li>resources</li>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Resources</li>
           </ul>
         </header>
-      </div>
+      </section>
     </div>
   );
 };
